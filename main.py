fdecc9e3f9476132c03653963cac4d988d7c3ddd
Fyodor Y <fygrave@gmail.com>
2024-04-20 14:56:37 +0100

    play around

--- main.py
+++ main.py
@@ -59,19 +59,19 @@ def app():
   footer()
   st.title("Graph Example")
   st.sidebar.title("Welcome")
-  query_type = st.sidebar.selectbox("Query Tpye: ", ["Inspirationals", "Marvel"]) # could add more stuff here later on or add other endpoints in the sidebar.
+  query_type = st.sidebar.selectbox("Intel Query: ", ["Searches", "Cases", "Huieises"]) # could add more stuff here later on or add other endpoints in the sidebar.
   config = Config(height=600, width=700, nodeHighlightBehavior=True, highlightColor="#F7A7A6", directed=True,
                   collapsible=True)
 
-  if query_type=="Inspirationals":
-    st.subheader("Inspirationals")
+  if query_type=="Searches":
+    st.subheader("Searches")
     with st.spinner("Loading data"):
       store = get_inspired()
       st.write("Nodes loaded: " + str(len(store.getNodes())))
     st.success("Done")
     agraph(list(store.getNodes()), (store.getEdges() ), config)
 
-  if query_type=="Marvel":
+  if query_type=="Cases":
     #based on http://marvel-force-chart.surge.sh/
     with open("./marvel.json", encoding="utf8") as f:
       marvel_file = json.loads(f.read())
